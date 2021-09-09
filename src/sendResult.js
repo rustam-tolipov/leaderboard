a7c429f77b04dad92f9f7b451f12f356eb799e19
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-09-09 17:49:39 +0500

    Chnage old ID to new ID

--- src/sendResult.js
+++ src/sendResult.js
@@ -1,15 +1,19 @@
-const sendResult = async (name, score, url) => {
-  const data = await fetch(url, {
+const saveToAPI = async (nameInput, scoreInput) => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9I998jQkwVXa66yLA2rE/scores', {
     method: 'POST',
+    body: JSON.stringify({
+      user: nameInput,
+      score: scoreInput,
+    }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-    body: JSON.stringify({
-      user: name,
-      score,
-    }),
   });
-  return data.json();
+  if (!response) {
+    throw new Error(`404! error: ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
 };
 
-export default sendResult;
+export default saveToAPI;
